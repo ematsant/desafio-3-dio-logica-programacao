1c6cbfb0e2cccf27b0f857071691fd92567660ad
ematsant <211806989+ematsant@users.noreply.github.com>
2026-05-07 21:51:52 -0300

    Created function attack in the class Hero, a decision structure for the name of the attack based on the type of hero, and tested it

--- index.js
+++ index.js
@@ -4,4 +4,26 @@ class Hero{
         this.age = age
         this.type = type
     }
+    attack(){
+        let attackName = ""
+        switch(this.type){
+            case "mage":
+                attackName = "magic"
+                break
+            case "warrior":
+                attackName = "a sword"
+                break
+            case "monk":
+                attackName = "martial arts"
+                break
+            case "ninja":
+                attackName = "a shuriken"
+                break
+            default:
+                attackName = "Unknown attack"
+        }
+        console.log(`The ${this.type} attacked using ${attackName}`)
+    }
 }
+const heroes = new Hero("Merlin", 100, "mage")
+heroes.attack()
