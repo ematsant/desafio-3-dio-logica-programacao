530f2ad2691c07281152e6b9dcfb94adfd737a04
ematsant <211806989+ematsant@users.noreply.github.com>
2026-05-07 21:58:18 -0300

    Código final e testado, conforme solicitado no desafio

--- index.js
+++ index.js
@@ -8,22 +8,31 @@ class Hero{
         let attackName = ""
         switch(this.type){
             case "mage":
-                attackName = "magic"
+                attackName = "magic."
                 break
             case "warrior":
-                attackName = "a sword"
+                attackName = "a sword."
                 break
             case "monk":
-                attackName = "martial arts"
+                attackName = "martial arts."
                 break
             case "ninja":
-                attackName = "a shuriken"
+                attackName = "a shuriken."
                 break
             default:
-                attackName = "Unknown attack"
+                attackName = "Unknown attack."
         }
         console.log(`The ${this.type} attacked using ${attackName}`)
     }
 }
-const heroes = new Hero("Merlin", 100, "mage")
-heroes.attack()
+const heroes = [
+    new Hero("Merlin", 100, "mage"),
+    new Hero("Achilles", 30, "warrior"),
+    new Hero("Benkei", 32, "monk"),
+    new Hero("Naruto", 19, "ninja")
+]
+console.log("\n***Start***\n")
+for (let hero of heroes){
+    hero.attack()
+}
+console.log("\n***End***\n")
